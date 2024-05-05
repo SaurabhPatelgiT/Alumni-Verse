7ae4a561fb1d448367f73c22a8512bcc36f2a18e
Saurabh Patel <saurabhpatel9836@gmail.com>
2024-05-05 10:25:27 +0530

    create and run the server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,16 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
-import SignIn from './pages/Signin';
+import Signin from './pages/Signin';
 import SignUp from './pages/SignUp';
 import Profile from './pages/Profile';
+import Header from './components/Header';
 
 export default function App() {
   return (
     <BrowserRouter>
+     {/* header */}
+     <Header />
     
       <Routes>
         <Route path='/' element={<Home />} />
